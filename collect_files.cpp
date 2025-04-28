0adc0e351f7500c4e96a97ce76a66cef11cdb372
turcheg16 <artyrkadolota@gmail.com>
2025-04-29 02:24:29 +0300

    Update collect_files.cpp

--- collect_files.cpp
+++ collect_files.cpp
@@ -1,2 +1,17 @@
+#include <filesystem>
 #include <iostream>
-int main(int argc,char*[]){return argc<3?1:0;}
+namespace fs=std::filesystem;
+int main(int argc,char*argv[]){
+    if(argc<3){return 1;}
+    fs::path src=argv[1],dst=argv[2];
+    if(!fs::is_directory(src)){return 1;}
+    fs::create_directories(dst);
+    std::size_t d0=std::distance(src.begin(),src.end());
+    for(const auto&e:fs::recursive_directory_iterator(src)){
+        if(!e.is_regular_file())continue;
+        std::size_t d=std::distance(src.begin(),e.path().begin())-d0;
+        if(d>2)continue;
+        fs::copy_file(e.path(),dst/e.path().filename(),fs::copy_options::overwrite_existing);
+    }
+    return 0;
+}
